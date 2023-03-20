041930db9acac8d38cf5ec2b31a1dfcda7cb3b5a
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2023-03-20 11:59:26 -0300

    feat: add utility function to typecheck unreachable states

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
 export function clamp(min: number, n: number, max: number): number {
   return Math.min(Math.max(min, n), max);
 }
+
+export function assertUnreachable(_: never): never {
+  throw new Error("Reached situation thought unreachable, all hope is lost");
+}
