3e823f1740e02769d05070b83ee78085faa9bf76
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2023-03-25 18:04:48 -0300

    feat: add utility function to indent yaml

--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,16 @@ export function clamp(min: number, n: number, max: number): number {
 export function assertUnreachable(_: never): never {
   throw new Error("Reached situation thought unreachable, all hope is lost");
 }
+
+export function indent(s: string, count: number = 2) {
+  if (count < 0) {
+    throw new RangeError(`Expected count to be at least 0, got ${count}`);
+  }
+
+  if (count === 0) {
+    return s;
+  }
+
+  const regex = /^(?!\s*$)/gm;
+  return s.replace(regex, " ".repeat(count));
+}
