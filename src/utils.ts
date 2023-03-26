da7f0b5b38c4fbd63a6ca015cbc42298e3c1a632
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2023-03-25 22:34:55 -0300

    feat: add function to check for graph cycles

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { Edge } from "reactflow";
+
 export function clamp(min: number, n: number, max: number): number {
   return Math.min(Math.max(min, n), max);
 }
@@ -17,4 +19,36 @@ export function indent(s: string, count: number = 2) {
 
   const regex = /^(?!\s*$)/gm;
   return s.replace(regex, " ".repeat(count));
+}
+
+export function containsCycle(edges: Edge[]): boolean {
+  const visited: { [node: string]: boolean } = {};
+  const stack: { [node: string]: boolean } = {};
+
+  const hasCycleHelper = (node: string): boolean => {
+    visited[node] = true;
+    stack[node] = true;
+
+    for (const edge of edges) {
+      if (edge.source === node) {
+        const target = edge.target;
+        if (!visited[target] && hasCycleHelper(target)) {
+          return true;
+        } else if (stack[target]) {
+          return true;
+        }
+      }
+    }
+
+    stack[node] = false;
+    return false;
+  };
+
+  for (const edge of edges) {
+    if (!visited[edge.source] && hasCycleHelper(edge.source)) {
+      return true;
+    }
+  }
+
+  return false;
 }
