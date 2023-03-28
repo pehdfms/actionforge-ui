ba974c6e2d68c917f9ed881f6149e758d44facec
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2023-03-28 15:47:52 -0300

    feat: make add button save changes to graph

--- src/components/cards/EventCard.tsx
+++ src/components/cards/EventCard.tsx
@@ -1,11 +1,33 @@
-import { useState } from "react";
-import { Handle, Position, NodeProps } from "reactflow";
-import { EventNode, PushEvent, Trigger } from "../../domain/events";
+import {
+  Handle,
+  Position,
+  NodeProps,
+  useNodes,
+  applyNodeChanges,
+  useReactFlow,
+} from "reactflow";
+import { EventNode, PushEvent } from "../../domain/events";
 import { AddButton } from "../AddButton";
 import { Item } from "../Item";
 
-export function EventCard({ data }: NodeProps<EventNode>) {
-  const [items, setItems] = useState<Trigger[]>([]);
+export function EventCard({ data, id }: NodeProps<EventNode>) {
+  const { setNodes } = useReactFlow();
+
+  const addTrigger = () => {
+    setNodes((nodes) =>
+      nodes.map((node) => {
+        console.log("got here");
+        if (node.id === id) {
+          node.data = {
+            ...node.data,
+            triggers: [...node.data.triggers, new PushEvent()],
+          };
+        }
+
+        return node;
+      })
+    );
+  };
 
   return (
     <>
@@ -14,15 +36,8 @@ export function EventCard({ data }: NodeProps<EventNode>) {
       {data.triggers.map((trigger) => (
         <Item name={trigger.name} key={trigger.name} />
       ))}
-      {items.map((trigger) => (
-        <Item name={trigger.name} key={trigger.name} />
-      ))}
 
-      <AddButton
-        onClick={() => {
-          setItems([...items, new PushEvent()]);
-        }}
-      />
+      <AddButton onClick={addTrigger} />
 
       <Handle type="source" position={Position.Right} id="a" />
     </>
